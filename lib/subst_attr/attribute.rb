ad632fd35ef2aaee92d86278ad663ed02a61b193
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2015-10-07 15:05:56 -0500

    Replace bang actuator with call actuator.

--- lib/subst_attr/attribute.rb
+++ lib/subst_attr/attribute.rb
@@ -1,7 +1,7 @@
 module SubstAttr
   module Attribute
     def self.define(target_class, attr_name, interface=nil)
-      ::Attribute::Define.! target_class, attr_name, :accessor do
+      ::Attribute::Define.(target_class, attr_name, :accessor) do
         Substitute.build interface
       end
     end
