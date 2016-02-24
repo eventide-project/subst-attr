7fc59478557e6e5ca84da9b0a39417d256402b72
Scott Bellware <sbellware@gmail.com>
2016-02-24 14:36:22 -0600

    Test bench contexts are strings

--- test/spec/activated_object_class.rb
+++ test/spec/activated_object_class.rb
@@ -4,7 +4,7 @@ module ActivatedObjectClass
   SubstAttr.activate
 end
 
-context ActivatedObjectClass do
+context "ActivatedObjectClass" do
   context "NullAddr is not activated for a specific class" do
     test "NullAttr is activated for the Object class" do
       assert(Object.is_a? SubstAttr::Macro)
