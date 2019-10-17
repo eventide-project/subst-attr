a20a1e7e46bf762d8196b5ae2c6a622453881d1b
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-17 16:32:54 -0500

    Parenthesis are added to assert_raises and refute_raises

--- test/automated/null_object/strict.rb
+++ test/automated/null_object/strict.rb
@@ -6,7 +6,7 @@ context "Null Object" do
 
     context "Invoking Methods Not Implemented on the Impersonated Class" do
       test "Is an error" do
-        assert_raises NoMethodError do
+        assert_raises(NoMethodError) do
           example.some_attr.some_method
         end
       end
